f0ee2ae3f511d4d0e266f5bef8fd3247af22c9c5
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2025-02-19 19:37:54 +0100

    Added json loader to load configurations from root, removed redundant dll import

---
+++ Server/HogWarp.Common/IO/ConfigurationLoader.cs
@@ -0,0 +1,32 @@
+﻿namespace HogWarp.Common.IO;
+
+public sealed class ConfigurationLoader
+{
+    private static JsonSerializerOptions JsonSerializerOptions { get; } = new()
+    {
+        PropertyNameCaseInsensitive = true,
+        IncludeFields = true,
+        NumberHandling = JsonNumberHandling.AllowReadingFromString
+    };
+
+    public static T? LoadFromJson<T>() where T : class
+    {
+        var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, typeof(T).Name.ToLower() + ".json");
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine($"File not found: {filePath}");
+            return null;
+        }
+
+        try
+        {
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<T>(json, JsonSerializerOptions);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error deserializing JSON: {ex.Message}");
+            return null;
+        }
+    }
+}
